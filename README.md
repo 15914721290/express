f34cfb395dfec2968edef19f435d8d0a0b536292
caixiaoping <amy.cai@100cb.cn>
2018-03-16 11:29:04 +0800

    gentou_seo框架搭建

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# express
+### 部署流程
+
+ 1. npm install/cnpm install
+ 2. npm install pm2 -g/cnpm install pm2 -g
+ 3. 打开控制台cd到项目目录，输入 npm run dev然后启动项目
+
+### 启动命令
+ * npm run start : 正式环境启动项目
+ * npm run dev : 开发环境启动项目
+ * npm run watch-dev : 开发环境webpack构建命令，带有监听自动打包功能
+ * npm run build : 正式环境webpack构建命令
+
+### 项目技术栈
+ * PC端：jQuery
+ * WAP端：zepto
+ * 后端：express
+ * 模板引擎：nunjucks
---
+++ webpack.develop.config.js
@@ -0,0 +1,77 @@
+// 打包的webpack 配置
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const env = process.env.NODE_ENV;
+
+const config = {
+  entry: {
+    // JS文件入口
+    '/js/home.js': './public/javascripts/home/index.js',
+    // CSS文件入口
+    '/css/home.css': './public/stylesheets/home.css',
+  },
+  output: {
+    path: path.join(__dirname, 'static'),
+    filename: '[name]',
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(env),
+    }),
+    new ExtractTextPlugin({
+      filename: '[name]',
+      allChunks: true,
+      ignoreOrder: true,
+    }),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: __dirname,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['latest'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'css-loader?-importLoaders=1',
+          use: 'postcss-loader',
+        }),
+      },
+      {
+        test: /\.json$/,
+        use: ['json-loader'],
+      },
+      {
+        test: /\.node$/,
+        use: ['node-loader'],
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+        },
+      },
+    ],
+  },
+  node: {
+    console: true,
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty',
+  },
+};
+
+module.exports = config;
+// 打包的webpack配置
